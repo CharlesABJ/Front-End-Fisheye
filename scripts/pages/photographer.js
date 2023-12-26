6e845c1c97b799e9e0e90bdd513b628dd4b2861e
Charles ABJ <charlesabj@charless-mbp.home>
2023-12-26 19:53:59 +0100

    Affichage des medias

--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -1,1 +1,59 @@
-//Mettre le code JavaScript lié à la page photographer.html
+// Obtenir l'ID du photographe à partir de l'URL
+function getPhotographerIdFromUrl() {
+  const urlParams = new URLSearchParams(location.search);
+  return urlParams.get("id");
+}
+
+// On vérifie si l'ID du photographe correspond à celui de la page
+function isCurrentPhotographer(id) {
+  return id === getPhotographerIdFromUrl();
+}
+
+async function getPhotographer(id) {
+  const apiPhotographer = "./data/photographers.json";
+  const response = await fetch(apiPhotographer);
+  let data = await response.json();
+  let photographers = data.photographers;
+  const photographer = photographers.find((p) => p.id.toString() === id);
+  return photographer;
+}
+
+async function displayPhotographerHeader(photographer) {
+  const photographerHeader = document.querySelector(".photograph-header");
+  const photographerPageModel = photographerPageTemplate(photographer);
+  photographerHeader.innerHTML = photographerPageModel.getHeaderDOM();
+}
+async function getPhotographerMedia(id) {
+  const apiPhotographer = "./data/photographers.json";
+  const response = await fetch(apiPhotographer);
+  let data = await response.json();
+  let photographersMedias = data.media.filter(
+    (m) => m.photographerId.toString() === id
+  );
+  return photographersMedias;
+}
+
+function displayPhotographerMedia(medias, photographerId) {
+  const galleryCards = document.querySelector(".gallery-cards");
+
+  medias.forEach((media) => {
+    galleryCards.insertAdjacentHTML(
+      "beforeend",
+      getGalleryCard(media, photographerId)
+    );
+  });
+}
+
+async function init() {
+  // Récupère les datas des photographes
+  const id = getPhotographerIdFromUrl();
+  const resultat = await getPhotographer(id);
+  displayPhotographerHeader(resultat);
+  const medias = await getPhotographerMedia(id);
+
+  //   const mediaFactory = new MediaFactory();
+  //   const media = mediaFactory.getMedia("img.jpg", "image");
+  displayPhotographerMedia(medias, id);
+}
+
+init();
--- scripts/templates/photographer.js
+++ scripts/templates/photographer.js
@@ -1,17 +1,34 @@
 function photographerTemplate(data) {
-    const { name, portrait } = data;
+  const { portrait, name, city, country, tagline, price, id } = data;
 
-    const picture = `assets/photographers/${portrait}`;
+  const picture = `assets/photographers/${portrait}`;
 
-    function getUserCardDOM() {
-        const article = document.createElement( 'article' );
-        const img = document.createElement( 'img' );
-        img.setAttribute("src", picture)
-        const h2 = document.createElement( 'h2' );
-        h2.textContent = name;
-        article.appendChild(img);
-        article.appendChild(h2);
-        return (article);
-    }
-    return { name, picture, getUserCardDOM }
+  function getUserCardDOM() {
+    const article = document.createElement("article");
+    const a = document.createElement("a");
+    const cover = document.createElement("div");
+    const img = document.createElement("img");
+    const h2 = document.createElement("h2");
+    const h3 = document.createElement("h3");
+    const p = document.createElement("p");
+    const span = document.createElement("span");
+    a.setAttribute("href", `photographer.html?id=${id}`);
+
+    cover.setAttribute("class", "cover");
+    img.setAttribute("src", picture);
+    h2.innerHTML = name;
+    h3.innerHTML = `${city}, ${country}`;
+    p.innerHTML = tagline;
+    span.innerHTML = `${price}€/jour`;
+
+    article.appendChild(a);
+    a.appendChild(cover);
+    a.appendChild(h2);
+    a.appendChild(h3);
+    a.appendChild(p);
+    a.appendChild(span);
+    cover.appendChild(img);
+    return article;
+  }
+  return { getUserCardDOM };
 }
